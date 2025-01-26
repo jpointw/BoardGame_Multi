b99a47f88e7cadbe2f02d13f8cc6dc432aa6262d
jawon <jpointw@gmail.com>
2025-01-27 02:33:16 +0900

    LocalPlayer Modified

--- Assets/@Scripts/UI/LocalBoardPlayer.cs
+++ Assets/@Scripts/UI/LocalBoardPlayer.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using Doozy.Runtime.UIManager.Components;
 using Fusion;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
 
-public class LocalBoardPlayer : BasePlayer
+public class LocalBoardPlayer : BasePlayer2
 {
 
     public bool interactEnabled = false;
@@ -23,84 +24,116 @@ public class LocalBoardPlayer : BasePlayer
     public UIButton EndTurnButton;
 
 
-    public override void Initialize(PlayerRef playerRef)
+    public override void Init(BasePlayerInfo basePlayerInfo)
     {
-        base.Initialize(playerRef);
+        base.Init(basePlayerInfo);
         EndTurnButton.onClickEvent.AddListener(EndTurn);
         ReservedCardButton.onClickEvent.AddListener(OpenReservedCardPanel);
+        for (int i = 0; i < selectedCoinsButtons.Length; i++)
+        {
+            selectedCoinsButtons[i].gameObject.SetActive(false);
+        }
     }
 
     public int CanCardPurchase(CardInfo card)
     {
         int requireSpecialCoins = 0;
 
-        for (int i = 0; i < OwnedCoins.Length; i++)
+        for (int i = 0; i < BasePlayerInfo.OwnedCoins.Length - 1; i++)
         {
-            requireSpecialCoins += Math.Max(0, card.cost[i] - OwnedCoins[i]);
+            requireSpecialCoins += Math.Max(0,BasePlayerInfo.OwnedCoins[i] - card.cost[i]);
         }
 
         return requireSpecialCoins;
     }
     
-    public void RequestPurchaseCard(CardElement cardElement)
+    public void RequestPurchaseCard(int cardId)
     {
-        if (!Object.HasInputAuthority) return;
-        GameSystem.Instance.HandlePurchaseRequest(PlayerRef, cardElement);
+        if (!interactEnabled) return;
+        GameSystem.Instance.RPC_HandlePurchaseRequest(BasePlayerInfo.PlayerRef, cardId);
     }
 
-    public void RequestReserveCard(CardInfo cardInfo)
+    public void RequestPurchaseReservedCard(int cardId)
     {
-        if (!Object.HasInputAuthority) return;
-        GameSystem.Instance.HandleReserveCardRequest(PlayerRef, cardInfo);
-        GameSystem.Instance.HandleTakeCoins(PlayerRef, new []{0,0,0,0,0,1});
+        if (!interactEnabled) return;
+        GameSystem.Instance.RPC_HandlePurchaseRequest(BasePlayerInfo.PlayerRef, cardId, true);
+        GameSystem.Instance.InGameUI.reservedCardDetailUI.Close();
+    }
+
+    public void RequestReserveCard(int cardId)
+    {
+        if (!interactEnabled) return;
+        GameSystem.Instance.RPC_HandleReserveCardRequest(BasePlayerInfo.PlayerRef, cardId);
+        GameSystem.Instance.RPC_HandleTakeCoins(BasePlayerInfo.PlayerRef, new []{0,0,0,0,0,1});
     }
 
     public void TakeSelectedCoins()
     {
-        if (!Object.HasInputAuthority) return;
-        GameSystem.Instance.HandleTakeCoins(PlayerRef, selectedCoins);
+        GameSystem.Instance.RPC_HandleTakeCoins(BasePlayerInfo.PlayerRef, selectedCoins);
+        for (int i = 0; i < selectedCoins.Length; i++)
+        {
+            selectedCoins[i] = 0;
+        }
+
+        for (int i = 0; i < selectedCoinsButtons.Length; i++)
+        {
+            selectedCoinsButtons[i].gameObject.SetActive(false);
+        }
     }
 
     public void OpenReservedCardPanel()
     {
-        if (!Object.HasInputAuthority) return;
-        GameSystem.Instance.InGameUI.ShowReservedCardsDetail(ReservedCards.ToArray());
+        GameSystem.Instance.InGameUI.ShowReservedCardsDetail(BasePlayerInfo.ReservedCards.ToArray());
         
     }
 
     private void EndTurn()
     {
-        if (!Object.HasInputAuthority) return;
-
+        if (!interactEnabled) return;
         interactEnabled = false;
         TakeSelectedCoins();
-        GameSystem.Instance.EndTurn(PlayerRef);
+        GameSystem.Instance.EndTurn(BasePlayerInfo.PlayerRef);
     }
     public void SelectCoin(int coinType)
     {
-        if (!Object.HasInputAuthority) return;
-        if (!CanSelectCoin(coinType)) return;
-        if (!CanTakeCoins()) return;
-        selectedCoins[coinType]++;
-        UpdateSelectedCoinUI();
-    }
-    
-    private bool CanSelectCoin(int coinType)
-    {
-        int differentCoins = selectedCoins.Count(c => c > 0);
-        if (differentCoins >= 3 && selectedCoins[coinType] == 0) return false;
-
-        if (selectedCoins[coinType] == 1)
+        if (!interactEnabled) return;
+        if (!CanSelectCoin(coinType))
         {
-            int availableCoins = GameSystem.Instance.CoinSystem.CentralCoins[coinType];
-            if (availableCoins < 4) return false;
+            Debug.LogError("Can't select coin");
+            return;
         }
 
-        return true;
+        if (!CanTakeCoins())
+        {
+            Debug.LogError("Can't take coins");
+            return;
+        }
+        selectedCoins[coinType]++;
+        GameSystem.Instance.InGameUI.UpdateTempCoinText(coinType);
+        UpdateSelectedCoinUI(coinType);
+    }
+    private bool CanSelectCoin(int coinType)
+    {
+        int differentCoinTypes = selectedCoins.Count(c => c > 0);   
+        int selectedTwoCoins = selectedCoins.Count(c => c > 1);     
+        int totalSelectedCoins = selectedCoins.Sum();               
+        int availableCoins = GameSystem.Instance.CoinSystem.CentralCoins[coinType]; 
+        
+        bool isMaxTotalCoins = (BasePlayerInfo.OwnedCoins.Sum() + totalSelectedCoins >= 10);
+        
+        bool cannotAddNewCoin = (selectedCoins[coinType] == 0 && (differentCoinTypes >= 3 
+                                                                  || selectedTwoCoins > 0));
+        
+        bool cannotAddSameCoin = (selectedCoins[coinType] == 1 && (availableCoins <= selectedCoins[coinType]
+                                                                   || differentCoinTypes > 1));
+        
+        bool isCoinLimitExceeded = (selectedCoins[coinType] >= 2);
+
+        return !(isMaxTotalCoins || cannotAddNewCoin || cannotAddSameCoin || isCoinLimitExceeded);
     }
     private bool CanTakeCoins()
     {
-        int totalCoinsAfterTake = OwnedCoins.Sum() + selectedCoins.Sum();
+        int totalCoinsAfterTake = BasePlayerInfo.OwnedCoins.Sum() + selectedCoins.Sum();
         
         if (totalCoinsAfterTake > 10) return false;
 
@@ -112,29 +145,30 @@ public class LocalBoardPlayer : BasePlayer
         interactEnabled = true;
     }
     
-    private void UpdateSelectedCoinUI()
+    private void UpdateSelectedCoinUI(int coinType)
+    {
+        var button = GetCoinButton();
+        button.GetComponent<Image>().sprite = UIDataBase.Instance.coinSprites[coinType];
+        button.onClick.RemoveAllListeners();
+        button.onClick.AddListener(() =>
+        {
+            selectedCoins[coinType]--;
+            button.gameObject.SetActive(false);
+        });
+        button.gameObject.SetActive(true);
+    }
+
+    private Button GetCoinButton()
     {
         for (int i = 0; i < selectedCoinsButtons.Length; i++)
         {
-            if (selectedCoins[i] > 0)
+            if (!selectedCoinsButtons[i].gameObject.activeSelf)
             {
-                selectedCoinsButtons[i].GetComponent<Image>().sprite = UIDataBase.Instance.coinSprites[i];
-                selectedCoinsButtons[i].onClick.RemoveAllListeners();
-
-                int index = i; // 클로저 개같은 거
-                selectedCoinsButtons[i].onClick.AddListener(() =>
-                {
-                    selectedCoins[index]--;
-                    UpdateSelectedCoinUI();
-                });
-
-                selectedCoinsButtons[i].gameObject.SetActive(true);
-            }
-            else
-            {
-                selectedCoinsButtons[i].gameObject.SetActive(false);
+                return selectedCoinsButtons[i]; 
             }
         }
+
+        return null;
     }
 
 }
