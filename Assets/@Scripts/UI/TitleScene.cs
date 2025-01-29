8bc2277601b49d06a30cf1db5b7b7b98e4fa6dbc
jawon <jpointw@gmail.com>
2025-01-30 02:34:18 +0900

    TitleScene Add

--- Assets/@Scripts/UI/TitleScene.cs
+++ Assets/@Scripts/UI/TitleScene.cs
@@ -1,19 +1,26 @@
 using System;
+using DG.Tweening;
 using UnityEngine;
 using TMPro;
 using Doozy.Runtime.UIManager.Components;
+using UnityEngine.UI;
 
 public class TitleScene : MonoBehaviour
 {
     [Header("UI Buttons")]
-    public UIButton playerAmountButton;
-    public UIButton scoreButton;
-    public UIButton timerButton;
+    public UIButton[] titleButtons;
+
+    [Header("Fill Amount Images")]
+    public Image[] fillAmountImages;
 
     [Header("UI Texts")]
-    public TMP_Text playerAmountText;
-    public TMP_Text scoreText;
-    public TMP_Text timerText;
+    public TMP_Text[] titleTexts;
+    
+    [Header("Game Start Buttons")]
+    public UIButton createRoomButton;
+    public UIButton findRoomButton;
+
+    private float animationDuration = 0.5f;
 
     private int[] playerAmounts = {2, 3, 4};
     private int[] victoryPoints = {12, 15, 18};
@@ -21,42 +28,63 @@ public class TitleScene : MonoBehaviour
 
     private void Start()
     {
+        var A = UIDataBase.Instance;
+        var B = CardModelData.Instance;
         GameSharedData.PlayerCount = 2;
         GameSharedData.GameVictoryPoints = 12;
         GameSharedData.PlayerTurnTime = 30;
 
-        UpdateUI();
+        UpdateUI(0, GameSharedData.PlayerCount, 2,4);
+        UpdateUI(1, GameSharedData.GameVictoryPoints,12, 18);
+        UpdateUI(2, GameSharedData.PlayerTurnTime,30, 90);
 
-        playerAmountButton.onClickEvent.AddListener(ChangePlayerAmount);
-        scoreButton.onClickEvent.AddListener(ChangeVictoryPoints);
-        timerButton.onClickEvent.AddListener(ChangeTurnTimer);
+        titleButtons[0].onClickEvent.AddListener(ChangePlayerAmount);
+        titleButtons[1].onClickEvent.AddListener(ChangeVictoryPoints);
+        titleButtons[2].onClickEvent.AddListener(ChangeTurnTimer);
+        
+        createRoomButton.onClickEvent.AddListener(CreateRoom);
+        findRoomButton.onClickEvent.AddListener(FindRoom);
     }
 
     private void ChangePlayerAmount()
     {
         int index = Array.IndexOf(playerAmounts, GameSharedData.PlayerCount);
         GameSharedData.PlayerCount = playerAmounts[(index + 1) % playerAmounts.Length];
-        UpdateUI();
+        UpdateUI(0, GameSharedData.PlayerCount, 2, 4);
     }
 
     private void ChangeVictoryPoints()
     {
         int index = Array.IndexOf(victoryPoints, GameSharedData.GameVictoryPoints);
         GameSharedData.GameVictoryPoints = victoryPoints[(index + 1) % victoryPoints.Length];
-        UpdateUI();
+        UpdateUI(1, GameSharedData.GameVictoryPoints, 12, 18);
     }
 
     private void ChangeTurnTimer()
     {
         int index = Array.IndexOf(turnTimers, GameSharedData.PlayerTurnTime);
         GameSharedData.PlayerTurnTime = turnTimers[(index + 1) % turnTimers.Length];
-        UpdateUI();
+        UpdateUI(2, GameSharedData.PlayerTurnTime, 30, 90);
     }
 
-    private void UpdateUI()
+
+    private void UpdateUI(int index, float currentValue, float minValue, float maxValue)
     {
-        playerAmountText.text = $"Players: {GameSharedData.PlayerCount}";
-        scoreText.text = $"Victory: {GameSharedData.GameVictoryPoints}";
-        timerText.text = $"Turn Time: {GameSharedData.PlayerTurnTime}s";
+        titleTexts[index].text = currentValue.ToString();
+
+        float targetValue = (currentValue - minValue) / (maxValue - minValue);
+
+        fillAmountImages[index].DOFillAmount(targetValue, animationDuration)
+            .SetEase(Ease.InOutQuad);
+    }
+
+    private async void CreateRoom()
+    {
+        NetworkSystem.Instance.CreateRoom();
+    }
+
+    private async void FindRoom()
+    {
+        NetworkSystem.Instance.FindRoom();
     }
 }
