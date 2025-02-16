b25b345c9bf80f640e8ffb6f7c33bd154baee20d
jawon <jpointw@gmail.com>
2025-02-16 12:16:02 +0900

    ReserveUI modified

--- Assets/@Scripts/UI/ReservedCardDetailUI.cs
+++ Assets/@Scripts/UI/ReservedCardDetailUI.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using Doozy.Runtime.UIManager.Components;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -10,18 +11,22 @@ public class ReservedCardDetailUI : MonoBehaviour
     public UIButton[] closeButtons;
     
     public ReservedCardElement[] reservedCardElements;
+
+    public RectTransform fillTransform;
     void Start()
     {
         foreach (var closeButton in closeButtons)
         {
             closeButton.onClickEvent.AddListener(Close);
         }
-
         Close();
     }
 
     public void Open(int[] cards)
     {
+        fillTransform.transform.localScale = Vector3.zero;
+        fillTransform.DOScale(1, 0.5f);
+        
         for (int i = 0; i < reservedCardElements.Length; i++)
         {
             reservedCardElements[i].gameObject.SetActive(false);
