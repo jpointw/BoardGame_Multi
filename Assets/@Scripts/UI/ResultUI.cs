67c59ffbb1421472b125d98938f986540889c00e
jawon <jpointw@gmail.com>
2025-02-17 12:16:20 +0900

    ResultUI modified

--- Assets/@Scripts/UI/ResultUI.cs
+++ Assets/@Scripts/UI/ResultUI.cs
@@ -1,6 +1,28 @@
+using System;
+using DG.Tweening;
+using Doozy.Runtime.UIManager.Components;
+using TMPro;
 using UnityEngine;
 
 public class ResultUI : MonoBehaviour
 {
-    
+    public Canvas thisCanvas;
+    public TMP_Text winnerText;
+
+    public RectTransform fillRectTransform;
+    public UIButton exitButton;
+
+    private void Start()
+    {
+        thisCanvas.enabled = false;
+    }
+
+    public void Open(string winnerName)
+    {
+        winnerText.text = winnerName;
+
+        fillRectTransform.transform.localScale = Vector3.zero;
+        thisCanvas.enabled = true;
+        fillRectTransform.transform.DOScale(1, 0.5f);
+    }
 }
